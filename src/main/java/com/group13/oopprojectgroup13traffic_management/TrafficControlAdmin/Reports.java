902158ad1eb7ae55434820815a87bf9e57533272
mobasshira13 <mobasshira8@gmail.com>
2025-11-16 22:56:39 +0600

    Reports commit

---
+++ src/main/java/com/group13/oopprojectgroup13traffic_management/TrafficControlAdmin/Reports.java
@@ -0,0 +1,47 @@
+package com.group13.oopprojectgroup13traffic_management.TrafficControlAdmin;
+
+public class Reports
+{
+    @javafx.fxml.FXML
+    private ComboBox selectReportTypeCB;
+    @javafx.fxml.FXML
+    private TableColumn reportsLocationTC;
+    @javafx.fxml.FXML
+    private TableColumn reportsDateTC;
+    @javafx.fxml.FXML
+    private TableColumn reportsCongestionLevelTC;
+    @javafx.fxml.FXML
+    private TextField searchtotalvehiclesTF;
+    @javafx.fxml.FXML
+    private DatePicker dpLD;
+    @javafx.fxml.FXML
+    private TableColumn reportsTrafficDencityTC;
+    @javafx.fxml.FXML
+    private TableView reportsTV;
+    @javafx.fxml.FXML
+    private TextField searchpeakhourTF;
+    @javafx.fxml.FXML
+    private DatePicker dpLDD;
+    @javafx.fxml.FXML
+    private TableColumn reportsIncidentsTC;
+    @javafx.fxml.FXML
+    private TextField searchtotalIncidentsTF;
+    @javafx.fxml.FXML
+    private TextField searchmostcongestedzoneTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void filterOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void exportaspdfOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generatereportOA(ActionEvent actionEvent) {
+    }
+}
